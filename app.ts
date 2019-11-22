9ca69b0c3eaff24983df6a9740063301a8eff94d
Robert Godwin <robertgodwin@Roberts-MacBook-Air.local>
2019-11-22 11:54:58 +0000

    End of Section 21

--- app.ts
+++ app.ts
@@ -1,51 +1,17 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//     name: 'Rob',
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-    ADMIN = 'ADMIN',
-    READ_ONLY = 100,
-    AUTHOR = 'AUTHOR'
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-const person = {
-    name: 'Rob',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
+const combinedAges = combine(30, 26);
 
-// person.role.push('admin');
-// person.role[1] = 10;
+console.log(combinedAges);
 
-// person.role = [0, 'admin', 'user'];
+const combinedNames = combine('Max', 'Anna');
 
-let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
-}
-
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+console.log(combinedNames);
