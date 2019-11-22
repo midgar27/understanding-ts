246f7865af2acef23f75f85726ab32bc581ca93a
Robert Godwin <robertgodwin@Roberts-MacBook-Air.local>
2019-11-22 18:56:15 +0000

    End of Section 26

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 printResult(add(5, 12));
+var combinedValues;
+combinedValues = add;
+console.log(combinedValues(8, 8));
--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+
+console.log(combinedValues(8, 8));
